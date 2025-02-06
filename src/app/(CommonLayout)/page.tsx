f0d185e0983d31298c10433d244060b69ce3f74b
AbabilGroup <itek.department@gmail.com>
2025-02-06 16:41:21 +0600

    consultation section

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,6 +1,8 @@
 import Achievements from "@/components/pages/homepage/Achievements";
+import BookConsultation from "@/components/pages/homepage/BookConsultation";
 import BrandLogos from "@/components/pages/homepage/BrandLogos";
 import Hero from "@/components/pages/homepage/Hero";
+import HowToHire from "@/components/pages/homepage/HowToHire";
 import TypeOfEmployees from "@/components/pages/homepage/TypeOfEmployees";
 import WhyChoose from "@/components/pages/homepage/WhyChoose";
 
@@ -12,6 +14,8 @@ const HomePage = () => {
       <WhyChoose />
       <BrandLogos />
       <TypeOfEmployees />
+      <HowToHire />
+      <BookConsultation />
     </main>
   );
 };
