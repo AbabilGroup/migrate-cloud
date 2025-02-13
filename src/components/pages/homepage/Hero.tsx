49a9e1a2e7b6afd43c3a1ee3ff18de314fbc2cee
AbabilGroup <itek.department@gmail.com>
2025-02-13 18:04:49 +0600

    remove hero padding

--- src/components/pages/homepage/Hero.tsx
+++ src/components/pages/homepage/Hero.tsx
@@ -12,7 +12,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <section className="flex min-h-[80vh] flex-col gap-10 px-[20px] py-20 xl:flex-row xl:items-center">
+    <section className="flex min-h-[80vh] flex-col gap-10 px-[20px] py-20 lg:px-0 xl:flex-row xl:items-center">
       <div className="flex basis-full flex-col items-center space-y-5 xl:basis-1/2 xl:items-start xl:pl-40 xl:pr-20">
         <h1 className="text-center text-5xl font-bold leading-[60px] xl:text-left">
           Simplify the process of hiring a global workforce.
